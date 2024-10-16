37bd9e261a7ca7114623214fa15354edaf7060fc
JL6666JL <1368301748@qq.com>
2024-10-16 15:10:13 +0800

    lab2 format

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -4,25 +4,22 @@ using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  return zero_point+static_cast<uint32_t>(n);
+  return zero_point + static_cast<uint32_t>( n );
 }
 
-uint64_t Wrap32::unwrap(Wrap32 zero_point, uint64_t checkpoint) const
+uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-    uint64_t uint_lowhalf=0x0000'0000'FFFF'FFFF ;
-    uint64_t uint_highhalf = 0xFFFF'FFFF'0000'0000 ;
-    uint64_t begin =uint_lowhalf + 1 ;
-    uint64_t n_low32 = this->raw_value_ - zero_point.raw_value_;
-    uint64_t c_low32 = checkpoint & uint_lowhalf;
-    uint64_t result = (checkpoint & uint_highhalf) | n_low32;
-    if (result >= begin && n_low32 > c_low32 && (n_low32 - c_low32) > (begin / 2)) {
-        return result - begin;
-    }
-    if (result < uint_highhalf && c_low32 > n_low32 && (c_low32 - n_low32) > (begin / 2)) {
-        return result + begin;
-    }
-    return result;
+  uint64_t uint_lowhalf = 0x0000'0000'FFFF'FFFF;
+  uint64_t uint_highhalf = 0xFFFF'FFFF'0000'0000;
+  uint64_t begin = uint_lowhalf + 1;
+  uint64_t n_low32 = this->raw_value_ - zero_point.raw_value_;
+  uint64_t c_low32 = checkpoint & uint_lowhalf;
+  uint64_t result = ( checkpoint & uint_highhalf ) | n_low32;
+  if ( result >= begin && n_low32 > c_low32 && ( n_low32 - c_low32 ) > ( begin / 2 ) ) {
+    return result - begin;
+  }
+  if ( result < uint_highhalf && c_low32 > n_low32 && ( c_low32 - n_low32 ) > ( begin / 2 ) ) {
+    return result + begin;
+  }
+  return result;
 }
-
-
-
